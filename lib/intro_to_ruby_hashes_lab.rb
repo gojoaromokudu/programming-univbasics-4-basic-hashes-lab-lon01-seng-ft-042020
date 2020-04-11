a78bfa9d1c707c9b04bf0efcf52daaa528d5f0f5
Gbenga Ojo-Aromokudu <gojoaromokudu@gmail.com>
2020-04-11 23:27:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,8 +9,6 @@ def my_hash
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  
   hash = {
     :name => 'Grace Hopper'
   }
@@ -19,4 +17,7 @@ end
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  hash = {
+    :id => number
+  }
 end
